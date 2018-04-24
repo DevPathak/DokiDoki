764401d9c09521bc82c1074dc5435be8585a010b
DevPathak <devpathak@github.com>
2018-04-24 13:22:11 +0530

    Setup
    seting the project with libgdx

---
+++ ProjectFolder/desktop/src/com/dokidoki/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dokidoki.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dokidoki.game.CoreCode;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CoreCode(), config);
+	}
+}
